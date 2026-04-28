73448e212d0b08eb29544bdb6f1ec7b4df1603bc
Bayram Naouar <bayram.naouar@gmail.com>
2026-04-28 01:02:39 -0400

    chore: update tuning parameters for final experiment run

--- src/models/registry.py
+++ src/models/registry.py
@@ -5,6 +5,6 @@ from src.models.one_class_svm import OneClassSVMModel
 # (class, X_file, tuning_sample_rate, final_sample_rate, need_input_dim)
 MODEL_REGISTRY = {
     "isolation_forest": (IsolationForestModel, "X_train.npy", 1.0, 1.0, False),
-    "one_class_svm": (OneClassSVMModel, "X_train.npy", 0.1, 0.5, False),
+    "one_class_svm": (OneClassSVMModel, "X_train.npy", 0.25, 0.5, False),
     "autoencoder": (AutoEncoderModel, "X_legit.npy", 1.0, 1.0, True),
 }
